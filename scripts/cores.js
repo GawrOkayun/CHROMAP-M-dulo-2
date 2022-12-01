a6600acfb6374825e48ee2797143ec143aa741d2
Bruno Henrique <brunohenrique.travessa@gmail.com>
2022-12-01 11:44:15 -0300

    modificando o script das cores

--- scripts/cores.js
+++ scripts/cores.js
@@ -1,6 +1,14 @@
 console.log("Carregando cores");
-
-const checkbox_Amarelo = document.getElementById("amarelo");
+const corpo_Html = document.querySelectorAll('html');
+const botao_Testar_Cores = document.querySelector('#testar-cores');
+const checkbox_Amarelo = document.getElementById('amarelo');
 const checkbox_Azul = document.getElementById("azul");
-const checkbox_Vermelho = document.getElementById("vermelho");
+const checkbox_Vermelho = document.getElementById('vermelho');
+
+botao_Testar_Cores.addEventListener('click', function(){
+
+        corpo_Html.classList.toggle('amarelo')
+        console.log("Adicionando amarelo")
+    }
+)
 
