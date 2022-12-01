fcf7f90f8a39694814c9143541e0caeb6d31854e
Navarone <brunohenrique.travessa@gmail.com>
2022-11-30 22:12:47 -0300

    teste de cores

---
+++ scripts/cores.js
@@ -0,0 +1,6 @@
+console.log("Carregando cores");
+
+const checkbox_Amarelo = document.getElementById("amarelo");
+const checkbox_Azul = document.getElementById("azul");
+const checkbox_Vermelho = document.getElementById("vermelho");
+
