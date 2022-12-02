597a51e32fbdb4590dd822ce2638129bab44d2fe
Navarone <brunohenrique.travessa@gmail.com>
2022-12-01 21:10:12 -0300

    upando a funcionalidade das cores

--- scripts/cores.js
+++ scripts/cores.js
@@ -1,14 +1,44 @@
 console.log("Carregando cores");
-const corpo_Html = document.querySelectorAll('html');
+const corpo_Html = document.querySelector('html');
 const botao_Testar_Cores = document.querySelector('#testar-cores');
 const checkbox_Amarelo = document.getElementById('amarelo');
 const checkbox_Azul = document.getElementById("azul");
 const checkbox_Vermelho = document.getElementById('vermelho');
 
 botao_Testar_Cores.addEventListener('click', function(){
-
-        corpo_Html.classList.toggle('amarelo')
-        console.log("Adicionando amarelo")
+    if(checkbox_Amarelo.checked){
+        if(checkbox_Vermelho.checked && checkbox_Azul.checked){
+            alert("Escolha apenas duas cores!")
+        }
+        else if(checkbox_Azul.checked){
+            corpo_Html.classList.add('verde');
+            console.log("Adicionando verde");
+        }
+        else if(checkbox_Vermelho.checked){
+            corpo_Html.classList.add('laranja');
+            console.log("Adicionando Laranja");
+        }
+        else{
+            corpo_Html.classList.add('amarelo');
+            console.log("Adicionando amarelo");
+        }
     }
+
+    else if(checkbox_Azul.checked){
+        if(checkbox_Vermelho.checked)
+        {
+            corpo_Html.classList.add('roxo');
+            console.log("Adicionando roxo");
+        }
+        else{
+            corpo_Html.classList.add('azul');
+            console.log("Adicionando azul");
+        }
+    }
+    else if(checkbox_Vermelho.checked){
+        corpo_Html.classList.add('vermelho');
+        console.log("Adicionando vermelho");
+    }
+}
 )
 
